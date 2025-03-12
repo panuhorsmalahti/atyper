2e807c6e1f36316d889e4c5f8f3d16bda99344d2
Panu Horsmalahti <phorsmalahti@mirantis.com>
2025-03-12 13:27:39 +0200

    add spinner to cli

--- frontends/cli/cli.ts
+++ frontends/cli/cli.ts
@@ -1,5 +1,6 @@
 import * as readline from "node:readline/promises";
 import { sendMessage } from "../vscode/src/shared/message";
+import * as ora from "ora";
 
 const startCli = async () => {
   const terminal = readline.createInterface({
@@ -9,10 +10,17 @@ const startCli = async () => {
 
   while (true) {
     const userInput = await terminal.question("you: ");
+
+    const spinner = ora.default({
+      discardStdin: false
+    }).start();
+
     const response = await sendMessage({
       role: "user",
       content: userInput,
     });
+
+    spinner.stop();
 
     console.log("atyper: " + response.content);
   }
